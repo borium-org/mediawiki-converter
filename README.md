1cb609e344b3b3598612d1e398e858968802a9f3
Borium <borium-org@github.com>
2022-01-22 17:46:53 -0600

    Basic project, input parameters.

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 This is a converter from MediaWiki pages to a static simplified web side that is suitable for hosting on GitHub pages.
 
-The test environment for this converter is the Borium web site that is maintained locally using Bitnami Mediawiki 1.37.1-0. Other versions may not work as well, and I don't intend to upgrade Mediawiki any time soon, so this environment will be stable enough for development.
+The test environment for this converter is the Borium web site that is maintained locally using Bitnami Mediawiki 1.37.1-0. Other versions may have pages in a layout that has unexpected elements, so other versions are not guaranteed to work. I don't intend to upgrade Mediawiki any time soon, so this environment will be stable enough for development.
+
+The converter in its first version is just reading in the input page line by line and dropping lines that should not be in the output file. No magic processing of HTML DOM or anything...
+
+# Usage
+
+This app assumes there is a running Mediawiki instance and the host has 'wget' utility available. The one I'm using is from CygWin package.
+
+1. Go to intermediate directory
+2. Execute wget:
+	wget --recursive --page-requisites --html-extension --convert-links --no-parent -R "*Special*" -R "*action=*" -R "*printable=*"  -R "*oldid=*" -R "*title=Talk:*" -R "*limit=*" http://foo.com/wiki/index.php/Main_Page
+3. Execute the converter app. I do it from Eclipse with default parameters coded into a response file:
+	MediaWikiConverter @ResponseFile
--- src/org/borium/mediawikiconverter/MediaWikiConverter.java
+++ src/org/borium/mediawikiconverter/MediaWikiConverter.java
@@ -1,9 +1,86 @@
 package org.borium.mediawikiconverter;
 
+import java.io.*;
+
 public class MediaWikiConverter
 {
+	private static String inputFolder;
+
+	private static String outputFolder;
+
+	private static String indexPage;
+
 	public static void main(String[] args)
 	{
+		if (args.length == 0)
+			args = new String[] { "@ResponseFile.txt" };
+		for (int i = 0; i < args.length; i++)
+		{
+			if (args[i].startsWith("@"))
+				readResponseFile(args[i].substring(1));
+			else
+			{
+				switch (args[i])
+				{
+				case "-input":
+					inputFolder = args[i + 1];
+					i++;
+					break;
+				case "-output":
+					outputFolder = args[i + 1];
+					i++;
+					break;
+				case "-index":
+					indexPage = args[i + 1];
+					i++;
+					break;
+				default:
+					throw new RuntimeException("Unrecognized parameter " + args[i]);
+				}
+			}
+		}
+		System.out.println("input:  " + inputFolder);
+		System.out.println("output: " + outputFolder);
+		System.out.println("index:  " + indexPage);
 		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Read parameters from a response file that is organized as a key-value pairs
+	 * separated with '=', with same keys as the input argument string array would
+	 * be.
+	 *
+	 * @param fileName File to read.
+	 */
+	private static void readResponseFile(String fileName)
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String line;
+			while ((line = br.readLine()) != null)
+			{
+				String[] split = line.split("=");
+				switch (split[0])
+				{
+				case "-input":
+					inputFolder = split[1];
+					break;
+				case "-output":
+					outputFolder = split[1];
+					break;
+				case "-index":
+					indexPage = split[1];
+					break;
+				default:
+					throw new RuntimeException("Unrecognized parameter " + split[0]);
+				}
+			}
+			br.close();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
